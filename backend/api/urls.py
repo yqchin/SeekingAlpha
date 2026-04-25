3cf2651e9e275e0fac789ea257ed98a0cb4b479e
Yung Qi Chin <yungqi.chin@fiuu.com>
2026-04-25 17:19:22 +0800

    changes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import external_api_call_view, db_test_view
+from .views import external_api_call_view, db_test_view, predict_credit_view
 
 urlpatterns = [
     path('call-external/', external_api_call_view, name='call-external'),
